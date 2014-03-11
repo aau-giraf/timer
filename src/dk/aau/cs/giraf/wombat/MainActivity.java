832f21558c3f2b7f33264c0cbd8aaf07148b44cb
Dennis Jakobsen <djakob11@student.aau.dk>
2014-03-11 13:51:29 +0100

    Commit for kunne push MainActivity.java

--- src/dk/aau/cs/giraf/wombat/MainActivity.java
+++ src/dk/aau/cs/giraf/wombat/MainActivity.java
@@ -7,9 +7,6 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import dk.aau.cs.giraf.TimerLib.Art;
 import dk.aau.cs.giraf.TimerLib.Guardian;
 import dk.aau.cs.giraf.oasis.lib.Helper;
@@ -29,8 +26,7 @@ public class MainActivity extends Activity {
 		long guardianId;
 		long childId;
 		int color;
-        String childName;
-        ProfilesHelper profileHelper = new ProfilesHelper(getApplicationContext());
+        ProfilesHelper profileHelper = new ProfilesHelper(this);
         Profile childProfile;
 
 		/* Get the data sent from the launcher (if there is any) */
@@ -41,21 +37,10 @@ public class MainActivity extends Activity {
         	color = extras.getInt("appBackgroundColor");
             childProfile = profileHelper.getProfileById(childId);
         } else {
-            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-
-            dlgAlert.setMessage("App'en skal startes gennem GIRAF");
-            dlgAlert.setTitle("Timer");
-            dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
-            dlgAlert.setCancelable(false);
-            dlgAlert.create().show();
-
         	guardianId = -1;
         	childId = -3;
         	color = getResources().getColor(R.color.GIRAFOrange);
+            childProfile = null;
         }
         
         ArrayList<Art> artList = new ArrayList<Art>();
@@ -83,9 +68,12 @@ public class MainActivity extends Activity {
 		findViewById(R.id.mainLayout).setBackgroundDrawable(d);
 
         // Set the name of the child in the customizeHeader TextView
-        //TextView tv = (TextView) findViewById(R.id.customizeHeader);
-        //CharSequence cs = childProfile.getFirstname() + " " + childProfile.getSurname();
-        //tv.setText(cs);
+        if(childProfile != null) {
+            TextView tv = (TextView) findViewById(R.id.customizeHeader);
+            CharSequence cs = childProfile.getFirstname() + " " + childProfile.getSurname();
+            tv.setText(cs);
+        }
+
 	}
 	
 	/**
